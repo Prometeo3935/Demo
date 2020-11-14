f5e0fbca6d329066669e4c2e121ba0c0bf37e31a
walter flores <floreswaltermauricio@gmail.com>
2020-11-14 20:40:35 -0300

    Primer cambio

---
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DemoController {	
+
+	@RequestMapping(value= "/cliente/{clienteId}", method = RequestMethod.GET)
+	public String index(@PathVariable String clienteId) {
+		return "El Numero de cliente es: " + clienteId;
+	}
+
+}
